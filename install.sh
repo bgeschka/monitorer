50a8504b7bb0cdd63673247eaaadbcfbcd868121
Björn Geschka <bjoern@geschka.org>
2018-08-21 23:03:44 +0200

    add installer add systemd init script

---
+++ install.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+DEFAULTDATADIR="/var/monitorer"
+DEFAULTLISTENIP="0.0.0.0"
+DEFAULTLISTENPORT="8000"
+
+if [ "$UID" != '0' ]; then
+	echo "this has to run as root"
+	echo "try again with sudo $0"
+	exit 1
+fi
+
+cat<<EOF
+
+___  ___            _ _                      
+|  \/  |           (_) |                     
+| .  . | ___  _ __  _| |_ ___  _ __ ___ _ __ 
+| |\/| |/ _ \| '_ \| | __/ _ \| '__/ _ \ \'__|
+| |  | | (_) | | | | | || (_) | | |  __/ |   
+\_|  |_/\___/|_| |_|_|\__\___/|_|  \___|_|   
+                     
+
+
+This script installs a systemd service file
+with adjusted paths to the installation in $pwd			
+                                             
+EOF
+
+
+printf 'continue?(y):'
+read x
+[ "$x" != "y" ] && exit 2
+
+systemctl stop monitorer.service 2>/dev/null
+
+#cp systemd.service /etc/systemd/system/monitorer.service:
+
+systemdinstallpath="/etc/systemd/system/monitorer.service"
+mpath="$(dirname "$(realpath ./main.js)" )"
+sed 's#{{mainpath}}#'"$mpath"'#g' systemd.service > $systemdinstallpath
+systemctl daemon-reload
+echo "installed $systemdinstallpath"
+
+
+cat<<EOF
+Monitorer has to store data like previous results,
+job configurations, mail settings etc
+EOF
+
+printf "Where do you want monitorer to listen on? (default $DEFAULTLISTENIP):"
+read listenip
+printf "which Port? (default $DEFAULTLISTENPORT):"
+read listenport
+
+printf "Where do you want to store the data(default $DEFAULTDATADIR ):"
+read p
+mkdir -p "${path:-$DEFAULTDATADIR}"
+
+sed 's#\/tmp#'"${path:-$DEFAULTDATADIR}"'#g' example.config.json > config.json
+sed -i 's#0.0.0.0#'"${listenip:-$DEFAULTLISTENIP}"'#g' config.json
+sed -i 's#8000#'"${listenport:-$DEFAULTLISTENPORT}"'#g' config.json
+
+
+printf 'Start monitorer now?(y)'
+read sn
+[ "$sn" == "y" ] && systemctl start monitorer.service
+
+printf 'Enable monitorer on system startup?(y)'
+read onboot
+[ "$onboot" == "y" ] && systemctl enable monitorer.service
+
+cat<<EOF
+     _                  _ 
+    | |                | |
+  __| | ___  _ __   ___| |
+ / _\` |/ _ \| '_ \ / _ \ |
+| (_| | (_) | | | |  __/_|
+ \__,_|\___/|_| |_|\___(_)
+                          
+
+If you made a mistake during installation
+just re-run $0 and adjust			  
+EOF
